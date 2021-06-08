175bd0b9b761eebaa624871884b42f0a6e87ad1c
Vitor Nunes <stecks10@gmail.com>
2021-06-08 11:49:32 -0300

    Tipos de Parâmetros

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 /**
  * Métodos HTTP:
  * 
@@ -9,9 +11,22 @@ const app = express();
  * POST: criar uma informação no back-end
  * PUT/PATCH: Alterar uma informção no back-end
  * DELETE: Deletar uma informção no back-end
+ * 
+ * 
+ * Tipos de parâmetros:
+ * 
+ * Query Params: Filtros e paginação
+ * Route Params: Identificar recursos (Atualizar/deletar)
+ * Rquest Body: Conteúdo na hora dcriar ou editar um recurso (JSON)
  */
 
 app.get('/projects', (request, response) => {
+    console.log(title);
+    console.log(owner);
+
+    console.log(title);
+    console.log(owner);
+
     return response.json([
         'Projeto 1',
         'Projeto 2',
@@ -19,6 +34,12 @@ app.get('/projects', (request, response) => {
 });
 
 app.post('/projects',  (request, response) => {
+    const {title, owner} = request.body;
+
+    console.log(title);
+    console.log(owner);;
+
+
     return response.json([
         'Projeto 1',
         'Projeto 2',
@@ -29,6 +50,10 @@ app.post('/projects',  (request, response) => {
 //http://localhost:3333/project/2
 
 app.put('/projects/:id',  (request, response) => {
+    const id = request.params;
+
+    console.log(id);
+
     return response.json([
         'Projeto 4',
         'Projeto 2',
