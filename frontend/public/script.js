95c89845503fdd389fbc587ae7af9fec3baa26b5
Vitor Nunes <stecks10@gmail.com>
2021-06-08 18:11:44 -0300

    Componentização

--- frontend/public/script.js
+++
@@ -1,6 +0,0 @@
-/* 
-    Babel: Converter (tranpilar ) código do react para um codigo que o browser entendar
-    WebPack: Pra cada tipo de arquivo (.js, css, png) eu vou converter o codigo de uma maneira diferente 
-
-    Loaders: babel-loader, css-loader, image-loader...
-*/
