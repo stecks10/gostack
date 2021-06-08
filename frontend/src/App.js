edf4f52409c7c2303a73fb34f6ec75b8c843c1ac
Vitor Nunes <stecks10@gmail.com>
2021-06-08 19:04:41 -0300

    Propriedades

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,23 @@ import React from 'react';
 import Header from './components/Header';
 
 function App() {
-    return (
-        <>
-        <Header />
-        <Header />
-        </>
-    );
+  return (
+    <>
+      <Header title="Homepage">
+        <ul>
+          <li>HomePage</li>
+          <li>Projects</li>
+        </ul>
+      </Header>
+      <Header title="projects">
+        <ul>
+          <li>HomePage</li>
+          <li>Projects</li>
+          <li>login</li>
+        </ul>
+      </Header>
+    </>
+  );
 }
 
 export default App;
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-export default function Header() {
-    return (
-        <header>
-          <h1>ReactJs</h1>
-        </header>
-    );
+export default function Header({ title, children }) {
+  return (
+    <header>
+      <h1>{title}</h1>
+
+      {children}
+    </header>
+  );
 }
