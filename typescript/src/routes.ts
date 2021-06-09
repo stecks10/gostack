cd43c1f21b7fea084afb708e4b57aec5dd89281b
Vitor Nunes <stecks10@gmail.com>
2021-06-09 15:35:22 -0300

    Tipando objetos e vetores

--- typescript/src/routes.ts
+++ typescript/src/routes.ts
@@ -1,5 +1,20 @@
 import {Request, Response} from 'express';
+import createUser from './services/CreateUser';
 
-export function helloWorld(resquest: Request, response: Response)  {
+// string, number, boolean, object, Array
+// interfaces 
+
+export function helloWorld(resquest: Request, response: Response){
+    const user = createUser({
+        email: 'vitor@gmail.com',
+        password: '123456',
+        techs: [
+            'Node.js', 
+            'reactJs', 
+            'React Native', 
+            {title: 'Javascript', experience: 100},
+    ],
+    });
+
     return response.json({ message: 'Hello World'});
 }
