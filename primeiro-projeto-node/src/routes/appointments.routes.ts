06d56937d9c27746bf02e8aee421f24cc8323a2d
Vitor Nunes <stecks10@gmail.com>
2021-06-10 10:11:35 -0300

    Model de Agendamentos

--- primeiro-projeto-node/src/routes/appointments.routes.ts
+++ primeiro-projeto-node/src/routes/appointments.routes.ts
@@ -1,14 +1,10 @@
 import { Router } from 'express';
-import { v4 as uuid } from 'uuid';
 import { startOfHour, parseISO, isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
 
 const appointmentsRoutes = Router();
 // http://localhost:3333/appointments
-interface Appointment {
-  id: string;
-  provider: string;
-  date: Date;
-}
+
 
 const appointments: Appointment[] = [];
 
@@ -27,11 +23,7 @@ appointmentsRoutes.post('/', (request, response ) => {
      }
 
 
-  const appointment = {
-   id: uuid(),
-   provider,
-   date:parsedDate,
-  };
+  const appointment = new Appointment(provider, parsedDate);
 
   appointments.push(appointment);
 
