fba696373a7b390617bd0f3d74066e96f20d5f1b
Vitor Nunes <stecks10@gmail.com>
2021-06-09 23:21:19 -0300

    Debugando NodeJS
    configurando eslint, e pretter

--- primeiro-projeto-node/src/server.ts
+++ primeiro-projeto-node/src/server.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 
-const app = express();
-    
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello gostack'});
-});
+import routes from './routes';
 
+const app = express();
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
-    console.log('🚀 Server strarted on port 3333!');
-})
+  console.log('🚀 Server started on port 3333');
+});
