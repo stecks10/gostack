d2423b69514377d65c7c71c6f9bd41d5e004c4e7
Vitor Nunes <stecks10@gmail.com>
2021-06-14 10:12:43 -0300

    Model e migration de usuários

--- backend/src/database/migrations/1623413789152-CreateAppointments.ts
+++ backend/src/database/migrations/1623413789152-CreateAppointments.ts
@@ -17,12 +17,20 @@ export default class CreateAppointments1623413789152 implements MigrationInterfa
             {
               name: 'provider',
               type: 'varchar',
-              isNullable: false,
+            },
+            {
+              name: 'created_at',
+              type: 'timestamp',
+              default: 'now()',
+            },
+            {
+              name: 'updated_at',
+              type: 'timestamp',
+              default: 'now()',
             },
             {
               name: 'date',
               type: 'timestamp with time zone',
-              isNullable: false,
             }
           ]
         })
