703c85b9a5199e7698ffeeaf6557be767c03aa78
Vitor Nunes <stecks10@gmail.com>
2021-06-17 22:17:59 -0300

    Conectando a API

--- projeto/src/pages/Dashboard/index.tsx
+++ projeto/src/pages/Dashboard/index.tsx
@@ -1,58 +1,69 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo ] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>
+    ): Promise<void> {
+    event.preventDefault();
+    // Adição de um novo repositorio
+    // consumir api do github
+    // salvar novo repositorio no estado
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt ="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="teste">
           <img
-            src="https://avatars.githubusercontent.com/u/80498486?v=4"
-            alt="Vitor Nunes"
+            src={repository.owner.avatar_url}
+            alt={repository.owner.login}
          />
          <div>
-           <strong>rocketseat/unform</strong>
-           <p>Easy peasy highly scalabe ReactJS & React Native forms!</p>
+           <strong>{repository.full_name}</strong>
+           <p>{repository.description}</p>
          </div>
 
          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars.githubusercontent.com/u/80498486?v=4"
-            alt="Vitor Nunes"
-         />
-         <div>
-           <strong>rocketseat/unform</strong>
-           <p>Easy peasy highly scalabe ReactJS & React Native forms!</p>
-         </div>
 
-         <FiChevronRight size={20} />
         </a>
-        <a href="teste">
-          <img
-            src="https://avatars.githubusercontent.com/u/80498486?v=4"
-            alt="Vitor Nunes"
-         />
-         <div>
-           <strong>rocketseat/unform</strong>
-           <p>Easy peasy highly scalabe ReactJS & React Native forms!</p>
-         </div>
-
-         <FiChevronRight size={20} />
-        </a>
+        ))}
       </Repositories>
     </>
     );
