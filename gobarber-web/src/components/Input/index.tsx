a69b8aa967028d1cc13b9d29d9df3582c7932930
Vitor Nunes <stecks10@gmail.com>
2021-07-02 21:31:28 -0300

    configurando rotes no signIn e signUp

--- gobarber-web/src/components/Input/index.tsx
+++ gobarber-web/src/components/Input/index.tsx
@@ -11,19 +11,18 @@ import { FiAlertCircle } from 'react-icons/fi';
 import { Container, Error } from './styles';
 
 
-import Tooltip from '../Tooltip';
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
   icon: React.ComponentType<IconBaseProps>;
 }
 
-const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest}) => {
+const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
   const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
 
-  const { fieldName, defaultValue, error, registerField} = useField(name);
+  const { fieldName, defaultValue, error, registerField } = useField(name);
 
   const handleInputFocus = useCallback(() => {
     setIsFocused(true);
@@ -45,7 +44,7 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest}) => {
 
   return (
     <Container isErrored={!!error} isFilled={isFilled} isFocused={isFocused}>
-      { Icon && <Icon size={20} /> }
+      {Icon && <Icon size={20} />}
       <input
         onFocus={handleInputFocus}
         onBlur={handleInputBlur}
@@ -53,11 +52,11 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest}) => {
         ref={inputRef}
         {...rest} />
 
-        {error && (
-          <Error title={error}>
-            <FiAlertCircle color="#c53030" size={20} />
-          </Error>
-        )}
+      {error && (
+        <Error title={error}>
+          <FiAlertCircle color="#c53030" size={20} />
+        </Error>
+      )}
     </Container>
   );
 };
--- gobarber-web/src/pages/SignIn/index.tsx
+++ gobarber-web/src/pages/SignIn/index.tsx
@@ -1,15 +1,24 @@
 import React, { useCallback, useRef } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+
 import { FormHandles } from '@unform/core';
+
 import { Form } from '@unform/web';
+
 import * as Yup from 'yup';
-import { Link } from 'react-router-dom';
+
+import { Link, useHistory } from 'react-router-dom';
+
+
+
 
 import { useAuth } from '../../hooks/auth';
+
 import { useToast } from '../../hooks/toast';
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
+
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
@@ -23,69 +32,112 @@ interface SignInFormData {
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
+
   const { signIn } = useAuth();
+
   const { addToast } = useToast();
 
-  const handleSubmit = useCallback(async (data: SignInFormData) => {
-    try {
-      formRef.current?.setErrors({});
 
-      const schema = Yup.object().shape({
-        email: Yup.string()
-          .email('Digite um e-mail válido')
-          .required('E-mail obrigatório'),
-        password: Yup.string().required('Senha obrigatória'),
-      });
-      await schema.validate(data, {
-        abortEarly: false,
-      });
 
-      await signIn({
-        email: data.email,
-        password: data.password,
-      });
-    } catch (err) {
-      if (err instanceof Yup.ValidationError) {
-        const errors = getValidationErrors(err);
 
-        formRef.current?.setErrors(errors);
+  const history = useHistory();
 
-        return;
+
+
+
+  const handleSubmit = useCallback(
+
+    async (data: SignInFormData) => {
+
+      try {
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .email('Digite um e-mail válido')
+            .required('E-mail obrigatório'),
+          password: Yup.string().required('Senha obrigatória'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        await signIn({
+
+          email: data.email,
+
+          password: data.password,
+
+        });
+
+
+
+
+        history.push('/dashboard');
+
+      } catch (err) {
+
+        if (err instanceof Yup.ValidationError) {
+
+          const errors = getValidationErrors(err);
+
+          formRef.current?.setErrors(errors);
+
+          return;
+        }
+
+        addToast({
+          type: 'error',
+          title: 'Erro na autenticação',
+          description: 'Ocorreu um erro ao fazer login, cheque as credenciais.',
+
+        });
+
       }
 
-      addToast({
-        type: 'error',
-        title: 'Error na autenticação',
-        description: 'Ocorreu um erro ao fazer login, chegue as credenciasis.',
-      });
-    }
-  }, [signIn, addToast],
+    },
+
+    [signIn, addToast, history],
+
   );
+
+
+
+
   return (
     <Container>
       <Content>
         <AnimationContainer>
           <img src={logoImg} alt="GoBarber" />
+
           <Form ref={formRef} onSubmit={handleSubmit}>
             <h1>Faça seu logon</h1>
+
             <Input name="email" icon={FiMail} placeholder="E-mail" />
+
             <Input
               name="password"
               icon={FiLock}
               type="password"
               placeholder="Senha"
             />
+
             <Button type="submit">Entrar</Button>
+
             <a href="forgot">Esqueci minha senha</a>
           </Form>
-          <Link to="signup">
+
+          <Link to="/signup">
             <FiLogIn />
             Criar conta
           </Link>
         </AnimationContainer>
       </Content>
+
       <Background />
     </Container>
   );
 };
+
 export default SignIn;
