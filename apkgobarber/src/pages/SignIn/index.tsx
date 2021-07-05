1eb224b4c9cde71adfe1f19923d12745604a4695
Vitor Nunes <stecks10@gmail.com>
2021-07-05 09:03:46 -0300

    Input & Button

--- apkgobarber/src/pages/SignIn/index.tsx
+++ apkgobarber/src/pages/SignIn/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { Container, Title } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Title, } from './styles';
 
 import logoImg from '../../assets/logo.png';
 
@@ -10,7 +13,13 @@ const SignIn: React.FC = () => {
     <Container>
       <Image source={logoImg} />
 
-      <Title>Faã seu logon</Title>
+      <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button onPress={() => { }}>Entrar</Button>
     </Container>
   );
 };
